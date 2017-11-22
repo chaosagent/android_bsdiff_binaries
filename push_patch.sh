96b74211dc5d5a8276d6ef1fff2fec9c51d77854
David Hou <david.hou314@gmail.com>
2017-11-21 21:08:41 -0800

    Fix bug

--- push_patch.sh
+++ push_patch.sh
@@ -16,23 +16,25 @@ if [ "$apk_exists" -eq "0" ] || [ ! -f "$prev_apk_path" ]; then
     echo "Pushing APK..."
     adb push "$apk_path" /data/local/prev_version.apk
     echo "Installing..."
-    adb shell 'pm install /data/local/prev_version.apk'
+    adb shell 'pm install -r /data/local/prev_version.apk'
 
     cp "$apk_path" "$prev_apk_path"
     exit
 fi
 
 echo "Generating patch..."
-LD_LIBRARY_PATH=$DIR ./bsdiff_host "$apk_path" "$prev_apk_path" /tmp/patch
+LD_LIBRARY_PATH=$DIR ./bsdiff_host "$prev_apk_path" "$apk_path" /tmp/patch
 
 echo "Pushing patch..."
 adb push /tmp/patch /data/local/patch
 
 echo "Applying patch..."
-adb shell '/data/local/bspatch /data/local/prev_version.apk /data/local/prev_version.apk /data/local/patch'
+adb shell '/data/local/bspatch /data/local/prev_version.apk /data/local/current_version.apk /data/local/patch'
 
 cp "$apk_path" "$prev_apk_path"
 
 echo "Installing..."
-adb shell 'pm install -r /data/local/prev_version.apk'
+adb shell 'pm install -r /data/local/current_version.apk'
 
+
+adb shell 'mv /data/local/current_version.apk /data/local/prev_version.apk'
